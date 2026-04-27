6ed7c73daf5e1f7ff6784254ff69e17206124abe
Soner22224Deniz <22224@uktc-bg.com>
2026-04-27 16:51:14 +0300

    Update index.php
    Modified the main page

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php include "includes/header.php"; ?>
 
-<style>
-.intro {
-    height: 100vh;
-    background: url('images/background/hero.jpg') center/cover no-repeat;
-    position: relative;
-}
-
-.overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-}
-
-.intro-content {
-    position: relative;
-    z-index: 2;
-}
-
-section {
-    padding: 80px 0;
-}
-</style>
-
 
 <section class="intro d-flex align-items-center justify-content-center text-white text-center">
     <div class="overlay"></div>
@@ -31,27 +7,50 @@ section {
     <div class="intro-content">
         <h1 class="display-3">Welcome</h1>
         <p class="lead">Short intro text...</p>
-        <a href="lessons.php" class="btn btn-primary mt-3">Get Started</a>
-    </div>
-</section>
-
-
-<section class="bg-light text-center">
-    <div class="container">
-        <h2>About Ski Culture</h2>
-        <p>Placeholder text...</p>
+        <a href="lessons.php" class="btn btn-primary mt-3">Book a Lesson</a>
     </div>
 </section>
 
 
 <section class="text-center">
     <div class="container">
-        <h2>Famous Skiers</h2>
+        <h2 class="mb-5">Famous Skiers</h2>
 
-        <div class="row mt-4">
-            <div class="col-md-4">Card</div>
-            <div class="col-md-4">Card</div>
-            <div class="col-md-4">Card</div>
+        <div class="row justify-content-center">
+
+            <div class="col-md-4 d-flex justify-content-center mb-4">
+                <div class="card">
+                    <img src="images/skiers/skier1.jpg" class="card-img-top" alt="Skier">
+                    <div class="card-body">
+                        <h5 class="card-title">Skier Name</h5>
+                        <p class="card-text">Short description...</p>
+                        <a href="skier.php?id=1" class="btn btn-primary">View Profile</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex justify-content-center mb-4">
+                <div class="card">
+                    <img src="images/skiers/skier2.jpg" class="card-img-top" alt="Skier">
+                    <div class="card-body">
+                        <h5 class="card-title">Skier Name</h5>
+                        <p class="card-text">Short description...</p>
+                        <a href="skier.php?id=2" class="btn btn-primary">View Profile</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex justify-content-center mb-4">
+                <div class="card">
+                    <img src="images/skiers/skier3.jpg" class="card-img-top" alt="Skier">
+                    <div class="card-body">
+                        <h5 class="card-title">Skier Name</h5>
+                        <p class="card-text">Short description...</p>
+                        <a href="skier.php?id=3" class="btn btn-primary">View Profile</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
@@ -60,16 +59,10 @@ section {
 <section class="bg-light text-center">
     <div class="container">
         <h2>Equipment Stores</h2>
-        <a href="#" class="btn btn-outline-dark m-2">Store 1</a>
-        <a href="#" class="btn btn-outline-dark m-2">Store 2</a>
-    </div>
-</section>
 
-
-<section class="text-center">
-    <div class="container">
-        <h2>Contact</h2>
-        <p>Phone: +000 000 000</p>
+        <a href="#" class="btn btn-outline-dark m-2">Visit Store</a>
+        <a href="#" class="btn btn-outline-dark m-2">Visit Store</a>
+        <a href="#" class="btn btn-outline-dark m-2">Visit Store</a>
     </div>
 </section>
 
