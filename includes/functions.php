de8288b8b2447eaae19d8531784c1425bea85e0b
Soner22224Deniz <22224@uktc-bg.com>
2026-05-14 17:28:50 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -3,33 +3,33 @@
 function getProducts() {
     return [
         1 => [
-            "name" => "Ski Boots",
-            "price" => 120,
+            "name" => "KORE 112 Ti Freeride Ski",
+            "price" => 849,
             "image" => "images/products/product1.jpg"
         ],
         2 => [
-            "name" => "Ski Jacket",
-            "price" => 200,
+            "name" => "Prodigy 4 Powder Ski",
+            "price" => 749,
             "image" => "images/products/product2.jpg"
         ],
         3 => [
-            "name" => "Ski Goggles",
-            "price" => 80,
+            "name" => "HERO ST TI KONECT Racing skis",
+            "price" => 1090,
             "image" => "images/products/product3.jpg"
         ],
         4 => [
-            "name" => "Ski Helmet",
-            "price" => 150,
+            "name" => "Powder Helmet",
+            "price" => 36,
             "image" => "images/products/product4.jpg"
         ],
         5 => [
-            "name" => "Ski Gloves",
-            "price" => 50,
+            "name" => "HELLY HANSEN SWIFT HT GLOVE",
+            "price" => 45,
             "image" => "images/products/product5.jpg"
         ],
         6 => [
-            "name" => "Ski Poles",
-            "price" => 70,
+            "name" => "QUICKSILVER Men's goggles",
+            "price" => 39,
             "image" => "images/products/product6.jpg"
         ]
     ];
